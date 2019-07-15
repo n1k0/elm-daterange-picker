6713016d698a73b02fd431fc3ca0545792024f84
Nicolas Perriault <nicolas@perriault.net>
2019-07-15 16:48:09 +0200

    Publish demo [ci skip]

--- app.js
+++ app.js
@@ -7134,7 +7134,7 @@ var allo_media$elm_daterange_picker$DateRangePicker$Calendar$view = function (co
 									]),
 								_List_fromArray(
 									[
-										A2(allo_media$elm_daterange_picker$DateRangePicker$Calendar$navLink, '🠜', prev),
+										A2(allo_media$elm_daterange_picker$DateRangePicker$Calendar$navLink, '◄', prev),
 										A2(
 										elm$html$Html$th,
 										_List_fromArray(
@@ -7147,7 +7147,7 @@ var allo_media$elm_daterange_picker$DateRangePicker$Calendar$view = function (co
 												elm$html$Html$text(
 												A3(allo_media$elm_daterange_picker$DateRangePicker$Helpers$shortMonth, elm$time$Time$utc, monthFormatter, target))
 											])),
-										A2(allo_media$elm_daterange_picker$DateRangePicker$Calendar$navLink, '🠞', next)
+										A2(allo_media$elm_daterange_picker$DateRangePicker$Calendar$navLink, '►', next)
 									])),
 								A2(
 								elm$html$Html$tr,
